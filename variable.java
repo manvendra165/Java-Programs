db748cc8e04fafa1027841f23491cdfcd61e8ac1
Manvendra Pratap Singh <68546415+manvendra165@users.noreply.github.com>
2021-06-26 21:57:30 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,29 @@
+ 
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		float b=5.0f;
+		double c=10.0;
+		// there are 8 primitive data type in java 
+		// 1. byte (1 byte)
+		// 2. short (2 byte)
+		// 3. int (4 byte)
+		// 4. float (4 byte)
+		// 5. long (8 byte)
+		// 6. double (8 byte)
+		// 7. char (2 byte)
+		// 8. boolean (1 byte)
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		boolean check=true;
+		System.out.println(check);
+
+		
+
+	}
+
+}
